be521cdd36464c52817ec1da36dc386eeb3061e1
Mitchell Johnson <mitchellazj@gmail.com>
2017-06-25 17:36:33 +1000

    Install Theme

---
+++ install_theme.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update && sudo apt-get install arc-flatabulous-theme
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-get update && sudo apt-get install papirus-icon-theme
+sudo apt install git vim
