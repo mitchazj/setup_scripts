304a6705cf5702caef7a155f52dbc12dc882ac8f
Mitchell Johnson <mitchazj@users.noreply.github.com>
2018-01-20 00:56:04 +1000

    Update Version & Fix Issues

--- install_nvm_yarn.sh
+++ install_nvm_yarn.sh
@@ -1,6 +1,8 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-source ~/.bash_profile
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
-nvm install 7.2.0
