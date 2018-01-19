dd95a9fd247e1a852d8dddd71b0622ee5f2868f9
Mitchell Johnson <mitchazj@users.noreply.github.com>
2018-01-20 01:02:58 +1000

    Update conda version

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,3 +1,7 @@
 mkdir downloads
-wget "https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh" -O ./downloads/anaconda.sh
-bash ./downloads/anaconda.sh
+wget "https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh" -O ./downloads/anaconda.sh
+bash ./downloads/anaconda.sh -b -p ~/.anaconda
+rm Anaconda3-4.2.0-Linux-x86_64.sh
+echo 'export PATH="~/.anaconda/bin:$PATH"' >> ~/.bashrc 
+source .bashrc
+conda update conda
