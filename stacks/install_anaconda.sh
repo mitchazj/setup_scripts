1d9f1afb6c0367ab49bc962743fd41ccc3769544
Mitchell Johnson <orangutandevelopment@gmail.com>
2018-01-21 00:00:17 +1000

    hmm

--- stacks/install_anaconda.sh
+++ stacks/install_anaconda.sh
@@ -1,7 +1,7 @@
-mkdir downloads
+mkdir ./downloads
 wget "https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh" -O ./downloads/anaconda.sh
 bash ./downloads/anaconda.sh -b -p ~/.anaconda
-rm Anaconda3-4.2.0-Linux-x86_64.sh
+rm ./downloads/anaconda.sh
 echo 'export PATH="~/.anaconda/bin:$PATH"' >> ~/.bashrc 
 source .bashrc
 conda update conda
