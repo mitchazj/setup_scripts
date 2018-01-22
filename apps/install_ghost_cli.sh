7523bfe44769ca1c299d41f54f199cecacff7110
Mitchell Johnson <orangutandevelopment@gmail.com>
2018-01-22 21:17:03 +1000

    Yeehaw

--- apps/install_ghost_cli.sh
+++ apps/install_ghost_cli.sh
@@ -1,4 +1,4 @@
-sudo apt-get install mariadb-server -y
+sudo apt-get install mysql-server -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
 sudo apt-get install -y nodejs
 sudo npm i -g ghost-cli
