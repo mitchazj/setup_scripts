001d5b0bda55462e3c01e9eccc1da8890ced3332
Mitchell Johnson <orangutandevelopment@gmail.com>
2018-01-22 21:38:51 +1000

    Ghost

--- apps/install_ghost_cli.sh
+++ apps/install_ghost_cli.sh
@@ -2,3 +2,6 @@ sudo apt-get install mysql-server -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
 sudo apt-get install -y nodejs
 sudo npm i -g ghost-cli
+sudo mkdir -p /var/www/ghost
+sudo chown $USER:$USER /var/www/ghost
+cd /var/www/ghost
